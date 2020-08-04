e97761fb6489b8221afb18cf6a385a5e22379bc4
Darwin Tantuco <dcrtantuco@gmail.com>
2020-08-04 17:21:11 +0800

    Add prettier and husky

--- tests/lib/rules/deny-words.js
+++ tests/lib/rules/deny-words.js
@@ -1,36 +1,36 @@
-"use strict";
+'use strict'
 
-const RuleTester = require("eslint").RuleTester;
-const rule = require("../../../lib/rules/deny-words");
+const RuleTester = require('eslint').RuleTester
+const rule = require('../../../lib/rules/deny-words')
 
 const parserOptions = {
   ecmaVersion: 2018,
-  sourceType: "module",
-};
+  sourceType: 'module',
+}
 
 const settings = {
-  denyWordList: ["tite", "fck"],
-};
+  denyWordList: ['tite', 'fck'],
+}
 
-const ruleTester = new RuleTester({ parserOptions, settings });
+const ruleTester = new RuleTester({ parserOptions, settings })
 const defaultErrors = (word) => [
   { message: `Word \`${word}\` is not allowed.` },
-];
+]
 
-ruleTester.run("deny-words", rule, {
+ruleTester.run('deny-words', rule, {
   valid: [{ code: "'Awesome string'" }],
   invalid: [
     {
       code: "'tite'",
-      errors: defaultErrors("tite"),
+      errors: defaultErrors('tite'),
     },
     {
       code: "'fck'",
-      errors: defaultErrors("fck"),
+      errors: defaultErrors('fck'),
     },
     {
-      code: "<div> tite </div>",
-      errors: defaultErrors("tite"),
+      code: '<div> tite </div>',
+      errors: defaultErrors('tite'),
     },
     {
       code: `
@@ -38,7 +38,7 @@ ruleTester.run("deny-words", rule, {
           tite
         </div>
       `,
-      errors: defaultErrors("tite"),
+      errors: defaultErrors('tite'),
     },
   ],
-});
+})
