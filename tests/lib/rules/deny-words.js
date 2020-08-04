711b87d20e179d8f96377af02e20b570de4a7641
Darwin Tantuco <dcrtantuco@gmail.com>
2020-08-04 17:40:37 +0800

    Support jsx texts

--- tests/lib/rules/deny-words.js
+++ tests/lib/rules/deny-words.js
@@ -6,6 +6,9 @@ const rule = require('../../../lib/rules/deny-words')
 const parserOptions = {
   ecmaVersion: 2018,
   sourceType: 'module',
+  ecmaFeatures: {
+    jsx: true,
+  },
 }
 
 const settings = {
@@ -18,7 +21,7 @@ const defaultErrors = (word) => [
 ]
 
 ruleTester.run('deny-words', rule, {
-  valid: [{ code: "'Awesome string'" }],
+  valid: [{ code: "'Awesome string'" }, { code: '<h1> Awesome string </h1>' }],
   invalid: [
     {
       code: "'tite'",
